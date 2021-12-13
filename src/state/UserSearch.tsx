2adeb386c32d73ed1ee555d597ee125ad18a0a91
root <root@LDN-9C2D1F3.usersad.everis.int>
2021-12-13 15:30:29 +0000

    type unions in state

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -8,7 +8,15 @@ const users = [
 
 const UserSearch: React.FC = () => {
     const [name, setName] = useState('');
-    const onClick = () => {};
+    const [user, setUser] = useState<{ name:string, age: number } | undefined >();
+
+
+    const onClick = () => {
+        const foundUser = users.find((user) => {
+            return user.name === name;
+        }); 
+        setUser(foundUser);
+    };
 
 
     return (
@@ -16,6 +24,11 @@ const UserSearch: React.FC = () => {
          User Search
         <input value={name} onChange={(e) => setName(e.target.value)} /> 
         <button onClick={onClick}>Find User</button>
+        <div>
+            {user && user.name}
+            {user && user.age}
+
+        </div>
     </div>
     );
 };
